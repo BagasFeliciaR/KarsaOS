df1e68564eaae4e5f05d0dda31493f0bef063bbb
Bagas Felicia Rakhim <145270492+BagasFeliciaR@users.noreply.github.com>
2025-08-27 20:27:31 +0700

    Update profiledef.sh

--- archiso/karsa/profiledef.sh
+++ archiso/karsa/profiledef.sh
@@ -1,1 +1,20 @@
+#!/usr/bin/env bash
+
+iso_name="karsaos"
+iso_label="KARSA_$(date +%Y%m)"
+iso_publisher="Bagas Felicia Rakhim <https://github.com/BagasFeliciaR>"
+iso_application="KarsaOS Live ISO"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86')
+
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+)
 
